297e39414d800968bcef9df3e9bfa5a0b75a27ac
Tomas Soltys <tomas.soltys@range-software.com>
2025-07-27 19:26:47 +0200

    Updated list of packages

--- prereqs_ubuntu.sh
+++ prereqs_ubuntu.sh
@@ -16,6 +16,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
 assert_success $? "Failed to install software-properties-common" || exit 2
 
 echo_i "Install tools, Qt and ffmpeg"
-DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config build-essential alien openssh-client rpm qt6-base-dev qt6-pdf-dev libqt6svg6-dev libgl1-mesa-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev ffmpeg
+DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config build-essential alien openssh-client cmake rpm qt6-base-dev qt6-pdf-dev qt6-httpserver-dev qt6-websockets-dev qt6-multimedia-dev linguist-qt6 qt6-tools-dev qt6-tools-dev libxkbcommon-x11-dev libqt6svg6-dev
 assert_success $? "Failed to install tools, Qt and ffmpeg" || exit 2
 
