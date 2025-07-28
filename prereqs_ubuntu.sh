b1f6f3c9ec28d6a1b518e2471c61c4e5e19b0d82
Tomas Soltys <tomas.soltys@range-software.com>
2025-07-28 18:09:11 +0200

    Added missing ubuntu packages

--- prereqs_ubuntu.sh
+++ prereqs_ubuntu.sh
@@ -16,6 +16,6 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
 assert_success $? "Failed to install software-properties-common" || exit 2
 
 echo_i "Install tools, Qt and ffmpeg"
-DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config build-essential alien openssh-client cmake rpm qt6-base-dev qt6-pdf-dev qt6-httpserver-dev qt6-websockets-dev qt6-multimedia-dev linguist-qt6 qt6-tools-dev qt6-tools-dev libxkbcommon-x11-dev libqt6svg6-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y pkg-config build-essential alien openssh-client cmake rpm qt6-base-dev qt6-pdf-dev qt6-httpserver-dev qt6-websockets-dev qt6-multimedia-dev linguist-qt6 qt6-tools-dev qt6-tools-dev libxkbcommon-x11-dev libqt6svg6-dev libxcb-cursor0 libxcb-cursor-dev
 assert_success $? "Failed to install tools, Qt and ffmpeg" || exit 2
 
